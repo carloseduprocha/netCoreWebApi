8ad6e01da4d16bf821943a6d35a91a222dd8dec8
Carlos Rocha <caeprocha@outlook.com>
2026-06-09 17:31:30 -0300

    Adicionado a pasta de configurações da infraestrutura

---
+++ source/Backend/Configurations/InfraConfig.cs
@@ -0,0 +1,50 @@
+﻿using Contatos.Data;
+using Contatos.Data.Repositories;
+using Contatos.Models.Interfaces;
+using Contatos.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contatos.Configurations
+{
+    public static class InfraConfig
+    {
+        public static IServiceCollection AddInfrastructureServices(
+        this IServiceCollection services,
+        IConfiguration configuration,
+        IWebHostEnvironment environment)
+        {
+
+            var connection = configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<AppDbContext>(options => options.UseSqlite(connection));
+
+            services.AddScoped<IContactRepository, ContactRepository>();
+            services.AddScoped<IContactServices, ContactService>();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("DefaultCorsPolicy", policy =>
+                {
+                    if (environment.IsDevelopment())
+                    {
+                        policy.AllowAnyOrigin()
+                              .AllowAnyMethod()
+                              .AllowAnyHeader();
+                    }
+                    else
+                    {
+                        var allowedOrigins = configuration.GetSection("Origens").Get<string[]>();
+
+                        if (allowedOrigins != null && allowedOrigins.Length > 0)
+                        {
+                            policy.WithOrigins(allowedOrigins)
+                                  .AllowAnyMethod()
+                                  .AllowAnyHeader();
+                        }
+                    }
+                });
+            });
+
+            return services;
+        }
+    }
+}
